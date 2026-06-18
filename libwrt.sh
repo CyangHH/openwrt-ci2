42235924736568370523d85afaf1c6eda99ee655
Cyang <5225890+CyangHH@users.noreply.github.com>
2026-06-18 12:14:46 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,8 +1,3 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
-# 改用https://github.com/QiuSimons/luci-app-daed
-rm -rf feeds/packages/net/daed
-rm -rf feeds/luci/applications/luci-app-daed
-# 改用https://github.com/QiuSimons/luci-app-daed
-git clone https://github.com/QiuSimons/luci-app-daed package/dae
